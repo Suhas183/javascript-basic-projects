34266c30e0e55735a4bf42ab004bc885aa6ded41
Suhas Herle <herlesuhas@gmail.com>
2023-01-28 10:45:47 +0530

    Some Changes

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -1,7 +1,7 @@
 // local reviews data
 const reviews = [
   {
-    id: 1,
+    id: 0,
     name: "susan smith",
     job: "web developer",
     img:
@@ -10,7 +10,7 @@ const reviews = [
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
-    id: 2,
+    id: 1,
     name: "anna johnson",
     job: "web designer",
     img:
@@ -19,7 +19,7 @@ const reviews = [
       "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
-    id: 3,
+    id: 2,
     name: "peter jones",
     job: "intern",
     img:
@@ -28,7 +28,7 @@ const reviews = [
       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
-    id: 4,
+    id: 3,
     name: "bill anderson",
     job: "the boss",
     img:
@@ -37,3 +37,43 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomeBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson(currentItem);
+});
+
+function showPerson(person){
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+prevBtn.addEventListener("click",() => {
+  currentItem--;
+  currentItem = currentItem < 0 ? reviews.length - 1: currentItem;
+  showPerson(currentItem);
+});
+
+nextBtn.addEventListener("click",() => {
+  currentItem++;
+  currentItem = currentItem > reviews.length - 1 ? 0 : currentItem;
+  showPerson(currentItem);
+});
+
+randomeBtn.addEventListener("click", () => {
+  currentItem = Math.floor(Math.random()*reviews.length); 
+  showPerson(currentItem);
+});
